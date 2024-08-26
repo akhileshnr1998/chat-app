2fa4ea3454cea5190811c7659c530d001959ffbb
Akhilesh Nair <akhilesh@keyvalue.systems>
2024-08-27 02:23:13 +0530

    scalable-chat-app

---
+++ apps/server/src/index.ts
@@ -0,0 +1,19 @@
+import http from "http"
+import SocketService from "./services/socket.service";
+
+async function init() {
+    const socketService = new SocketService();
+
+    const httpServer = http.createServer();
+    const PORT = process.env.PORT || 8000;
+
+    socketService.io.attach(httpServer);
+
+    httpServer.listen(PORT, () => {
+        console.log(`Server listening to port: ${PORT}`)
+    })
+
+    socketService.initListeners();
+}
+
+init();
---
+++ apps/server/src/services/socket.service.ts
@@ -0,0 +1,54 @@
+import { Server } from "socket.io";
+import Redis from "ioredis";
+
+const publisher = new Redis({
+    host: "localhost",
+    port: 6379
+});
+
+const subscriber = new Redis({
+    host: "localhost",
+    port: 6379
+});
+
+class SocketService {
+    private server: Server;
+    constructor() {
+        console.log("Initialise Socket Service");
+        this.server = new Server({
+            cors: {
+                allowedHeaders: ['*'],
+                origin: '*'
+            }
+        });
+
+        subscriber.subscribe("MESSAGES");
+    }
+
+    get io() {
+        return this.server;
+    }
+
+    initListeners() {
+        const server = this.server;
+
+        server.on("connection", (socket) => {
+            console.log(`New socket connected`, socket.id);
+            socket.on("event:message", async ({message}: {message: string})=> {
+                console.log("New message delivered: ", message);
+
+                await publisher.publish("MESSAGES", JSON.stringify({ message}));
+            });
+        });
+
+        subscriber.on("message", (channel, message) => {
+            if (channel === "MESSAGES"){
+                server.emit("message", message);
+            }
+        })
+
+        console.log("Initialised Socket Listeners")
+    }
+}
+
+export default SocketService;
